eeb5aa1cc4e1ae539939f125afc3db24da01b942
Anh Mai <abmai91@gmail.com>
2021-06-24 22:18:20 -0700

    Add axios and wire up to backend

--- src/RequestInviteForm.tsx
+++ src/RequestInviteForm.tsx
@@ -1,7 +1,8 @@
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { useState } from "react";
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useState } from 'react';
+import axios from 'axios';
 
 interface FormInputs {
   fullName: string;
@@ -10,49 +11,74 @@ interface FormInputs {
 }
 
 interface RequestState {
-  status: 'requesting' | 'success' | 'failure',
-  errorMessage?: string
+  status: 'requesting' | 'success' | 'failure';
+  errorMessage?: string;
 }
 
 interface Props {
-  onFinish: () => void
+  onFinish: () => void;
 }
 
 const schema = yup.object().shape({
-  fullName: yup.string().required("Name is required."),
+  fullName: yup.string().required('Name is required.'),
   email: yup
     .string()
-    .email("Must be a valid email.")
-    .required("Email is required."),
+    .email('Must be a valid email.')
+    .required('Email is required.'),
   confirmEmail: yup
     .string()
     // Value is either null or must match previous email field
-    .oneOf([null, yup.ref("email")], "Emails must match."),
+    .oneOf([null, yup.ref('email')], 'Emails must match.'),
 });
 
 export default function RequestInviteForm(props: Props) {
   const [requestState, setRequestState] = useState<RequestState | undefined>();
 
   const { register, handleSubmit, formState } = useForm<FormInputs>({
-    mode: "onBlur",
+    mode: 'onBlur',
     resolver: yupResolver(schema),
   });
 
   const { errors } = formState;
 
-  const onSubmit = handleSubmit((data: FormInputs) => {
-    // TODO: implement
-    setRequestState({status: 'failure', errorMessage: 'Something went wrong!'});
+  const onSubmit = handleSubmit(async (data: FormInputs) => {
+    const { fullName, email } = data;
+    setRequestState({
+      status: 'requesting',
+    });
+    try {
+      await axios({
+        method: 'POST',
+        url: 'https://l94wc2001h.execute-api.ap-southeast-2.amazonaws.com/prod/fake-auth',
+        data: {
+          name: fullName,
+          email,
+        },
+      });
+      setRequestState({
+        status: 'success',
+      });
+    } catch ({ message, response }) {
+      setRequestState({
+        status: 'failure',
+        // Grab the response error message if there is one
+        // Otherwise default to the message in the generic error object
+        errorMessage: response?.data?.errorMessage ?? message,
+      });
+    }
   });
 
   function renderSuccess() {
     return (
       <>
         <h1>All done!</h1>
-        <p>Thank you for joining the waitlist. We will let you know as soon as you are able to try the app.</p>
+        <p>
+          Thank you for joining the waitlist. We will let you know as soon as
+          you are able to try the app.
+        </p>
         <button onClick={() => props.onFinish()}>Ok</button>
       </>
-    )
+    );
   }
 
   function renderForm() {
@@ -64,13 +90,13 @@ export default function RequestInviteForm(props: Props) {
             <input
               id="fullName"
               placeholder="Full Name"
-              {...register("fullName")}
+              {...register('fullName')}
             />
           </label>
           <p>{errors.fullName?.message}</p>
 
           <label htmlFor="email">
-            <input id="email" placeholder="Email" {...register("email")} />
+            <input id="email" placeholder="Email" {...register('email')} />
           </label>
           <p>{errors.email?.message}</p>
 
@@ -78,12 +104,17 @@ export default function RequestInviteForm(props: Props) {
             <input
               id="confirmEmail"
               placeholder="Confirm Email"
-              {...register("confirmEmail")}
+              {...register('confirmEmail')}
             />
           </label>
           <p>{errors.confirmEmail?.message}</p>
 
-          <input type="submit" />
+          <button
+            type="submit"
+            disabled={requestState?.status === 'requesting'}
+          >
+            {requestState?.status === 'requesting' ? 'Sending...' : 'Submit'}
+          </button>
           <p>{requestState?.errorMessage}</p>
         </form>
       </>
@@ -91,5 +122,4 @@ export default function RequestInviteForm(props: Props) {
   }
 
   return requestState?.status === 'success' ? renderSuccess() : renderForm();
-
 }
